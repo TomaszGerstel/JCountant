2cb4b441624b07e4f8aa9e9796f2a285f3c30448
TomaszGerstel <74785453+TomaszGerstel@users.noreply.github.com>
2022-12-29 11:23:55 +0100

    Delete bin directory

--- bin/README.md
+++
@@ -1,5 +0,0 @@
-# JCountant
-
-App to help you with your finances
-
-Java version of the tool to managing your finances
--- bin/src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-
-
-insert into `user_role` (name, description) values ('USER', 'basic authentication role');
-insert into `user_role` (name, description) values ('ADMIN', 'admin authentication role');
-insert into `user` (username, email, password) values ('admin', 'dm@gm.com','$2a$10$pxWU5Yhaf1gPACSjbanCsOGHMjJSTTeFrJiVle.3CChWnVALXW6lq');
-insert into `user_roles` (user_id, role_id) values (1, 1);
