270506944f9a2253595272a512985f4eb0cf721c
Tomasz <gerstel.tomasz@gmail.com>
2023-01-23 20:53:51 +0100

    transferCtrl some tests

---
+++ src/test/java/com/tgerstel/controller/TransferControllerMvcMockTest.java
@@ -0,0 +1,114 @@
+package com.tgerstel.controller;
+
+import static org.mockito.ArgumentMatchers.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.util.MultiValueMap;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.tgerstel.model.Receipt;
+import com.tgerstel.model.Transfer;
+import com.tgerstel.model.TransferType;
+import com.tgerstel.service.TransferService;
+
+@WebMvcTest(value = TransferController.class, excludeAutoConfiguration = { SecurityAutoConfiguration.class })
+@ExtendWith(MockitoExtension.class)
+public class TransferControllerMvcMockTest {
+
+	@MockBean
+	TransferService transferService;
+	
+	@Autowired
+	private MockMvc mockMvc;
+
+	static LocalDate dateTime;
+	static Receipt receipt;
+	static Transfer transfer;
+	static Transfer specialTransfer;
+	static Transfer notValidTransfer;
+
+	@BeforeAll
+	static void prepareVariables() {
+
+		dateTime = LocalDate.now();
+		receipt = new Receipt(dateTime, BigDecimal.valueOf(1200), BigDecimal.valueOf(1000.0), null, null, "Customer",
+				"Me", "for example", null);
+		transfer = new Transfer(TransferType.IN_TRANSFER, BigDecimal.valueOf(1200), "Customer", "Me", dateTime, null,
+				receipt, null);
+		transfer.setId(12L);
+		specialTransfer = new Transfer(TransferType.SALARY, BigDecimal.valueOf(1200), "Customer", "Me", dateTime, null,
+				null, null);
+		notValidTransfer = new Transfer(TransferType.TAX_OUT_TRANSFER, BigDecimal.valueOf(200), "Customer", "Me", null, null,
+				null, null);
+	}
+
+	@Test
+	void addTransferTest() throws Exception {
+
+		Mockito.when(transferService.createTransfer(any(), any(), any())).thenReturn(Optional.of(transfer));
+		
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/transfer").content(asJsonString(transfer)).param("receiptId", "2")
+				.contentType(MediaType.APPLICATION_JSON_VALUE).accept(MediaType.APPLICATION_JSON_VALUE))
+				.andExpect(MockMvcResultMatchers.status().isCreated())
+				.andExpect(MockMvcResultMatchers.content().json("{'amount': 1200.00}"))
+				.andExpect(MockMvcResultMatchers.content().json("{'id': 12}"))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.id").exists());
+	}
+	
+	@Test
+	@DisplayName("AddTransfer with regular transfer type without receipt_id should return 422 status")
+	void addTransferTest2() throws Exception {
+
+		Mockito.when(transferService.createTransfer(any(), any(), any())).thenReturn(Optional.of(transfer));
+		
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/transfer").content(asJsonString(transfer))
+				.contentType(MediaType.APPLICATION_JSON_VALUE).accept(MediaType.APPLICATION_JSON_VALUE))
+				.andExpect(MockMvcResultMatchers.status().isUnprocessableEntity());
+	}
+	
+	@Test
+	@DisplayName("AddTransfer with no valid transfer")
+	void addTransferTest3() throws Exception {
+
+		Mockito.when(transferService.createTransfer(any(), any(), any())).thenReturn(Optional.of(notValidTransfer));
+		
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/transfer").content(asJsonString(notValidTransfer))
+				.contentType(MediaType.APPLICATION_JSON_VALUE).accept(MediaType.APPLICATION_JSON_VALUE))			
+				.andExpect(MockMvcResultMatchers.status().isUnprocessableEntity());
+	}	
+
+	public static String asJsonString(final Object obj) {
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.registerModule(new JavaTimeModule());
+		mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+		try {
+			return mapper.writeValueAsString(obj);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+}
